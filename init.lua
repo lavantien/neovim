fc29fd1af398fd1b669260866420d3dfb3d44b30
lavantien <lavantien96@gmail.com>
2025-07-19 16:09:26 +0700

    add gopls and shared capabilities

--- init.lua
+++ init.lua
@@ -1,4 +1,17 @@
 require('options')
+
+vim.lsp.config('*', {
+    capabilities = {
+        textDocument = {
+            semanticTokens = {
+                multilineTokenSupport = true,
+            }
+        }
+    },
+    root_markers = { '.git' },
+})
+
 require('lsp')
 
 vim.lsp.enable('luals')
+vim.lsp.enable('gopls')
---
+++ readme.md
@@ -0,0 +1,6 @@
+# vanilla and sane config for neovim
+
+## todos
+
+- [ ] coloscheme and make treesitter working
+- [ ] auto format on save
