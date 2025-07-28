e2b311e409753f25c0754b7a15e3a4369ff61bed
lavantien <lavantien96@gmail.com>
2025-07-28 13:43:25 +0700

    migrate to a single init.lua and lspconfig

--- init.lua
+++ init.lua
@@ -1,4 +1,40 @@
-require('options')
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+vim.opt.winborder = 'rounded'
+vim.opt.inccommand = 'split'
+vim.opt.signcolumn = 'yes'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.expandtab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.g.mapleader = " "
+vim.g.loaded_netrw = 0
+vim.g.loaded_netrwPlugin = 0
+vim.g.have_nerd_font = true
+
+vim.pack.add({
+    { src = 'https://github.com/vague2k/vague.nvim' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/stevearc/oil.nvim' },
+    { src = 'https://github.com/echasnovski/mini.pick' },
+    { src = 'https://github.com/chomosuke/typst-preview.nvim' },
+})
+
+vim.lsp.enable({ 'lua_ls', 'gopls', 'rust_analyzer', 'pyright', 'ts_ls', 'jdtls', 'csharp_ls', 'tinymist', 'dockerls',
+    'docker_compose_language_service', 'yamlls', 'codebook' })
 
 vim.lsp.config('*', {
     capabilities = {
@@ -16,21 +52,98 @@ vim.lsp.config('*', {
     root_markers = { '.git' },
 })
 
-vim.lsp.enable('luals')
-vim.lsp.enable('gopls')
-vim.lsp.enable('tsls')
-vim.lsp.enable('pyright')
-vim.lsp.enable('csharpls')
-vim.lsp.enable('jdtls')
-vim.lsp.enable('rustanal')
-vim.lsp.enable('mdoxide')
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+            vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+            vim.keymap.set('i', '<C-Space>', function()
+                vim.lsp.completion.get()
+            end)
+        end
+    end,
+})
+vim.cmd("set completeopt+=noselect")
 
-require('lsp')
+vim.diagnostic.config({
+    virtual_lines = { current_line = true },
+})
 
--- waiting for 0.12
--- vim.pack.add({
---     "https://github.com/zenbones-theme/zenbones.nvim",
--- })
+require('mini.pick').setup()
+require('oil').setup()
+require('typst-preview').setup()
+
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+vim.keymap.set('n', '<leader>b', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>f', MiniPick.builtin.files)
+vim.keymap.set('n', '<leader>g', MiniPick.builtin.grep_live)
+vim.keymap.set('n', '<leader>h', MiniPick.builtin.help)
+vim.keymap.set('n', '<leader>p', ':TypstPreviewToggle<CR>')
+
+vim.cmd('colorscheme vague')
+vim.cmd(':hi statusline guibg=NONE')
+
+-- plugins hidden keymaps
+
+-- oil
+
+-- keymaps = {
+--     ["g?"] = { "actions.show_help", mode = "n" },
+--     ["<CR>"] = "actions.select",
+--     ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+--     ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+--     ["<C-t>"] = { "actions.select", opts = { tab = true } },
+--     ["<C-p>"] = "actions.preview",
+--     ["<C-c>"] = { "actions.close", mode = "n" },
+--     ["<C-l>"] = "actions.refresh",
+--     ["-"] = { "actions.parent", mode = "n" },
+--     ["_"] = { "actions.open_cwd", mode = "n" },
+--     ["`"] = { "actions.cd", mode = "n" },
+--     ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+--     ["gs"] = { "actions.change_sort", mode = "n" },
+--     ["gx"] = "actions.open_external",
+--     ["g."] = { "actions.toggle_hidden", mode = "n" },
+--     ["g\\"] = { "actions.toggle_trash", mode = "n" },
+-- }
+
+-- mini.pick
+
+-- mappings = {
+--     caret_left        = '<Left>',
+--     caret_right       = '<Right>',
 --
--- require('zenbones').setup()
-vim.cmd("colorscheme zenbones")
+--     choose            = '<CR>',
+--     choose_in_split   = '<C-s>',
+--     choose_in_tabpage = '<C-t>',
+--     choose_in_vsplit  = '<C-v>',
+--     choose_marked     = '<M-CR>',
+--
+--     delete_char       = '<BS>',
+--     delete_char_right = '<Del>',
+--     delete_left       = '<C-u>',
+--     delete_word       = '<C-w>',
+--
+--     mark              = '<C-x>',
+--     mark_all          = '<C-a>',
+--
+--     move_down         = '<C-n>',
+--     move_start        = '<C-g>',
+--     move_up           = '<C-p>',
+--
+--     paste             = '<C-r>',
+--
+--     refine            = '<C-Space>',
+--     refine_marked     = '<M-Space>',
+--
+--     scroll_down       = '<C-f>',
+--     scroll_left       = '<C-h>',
+--     scroll_right      = '<C-l>',
+--     scroll_up         = '<C-b>',
+--
+--     stop              = '<Esc>',
+--
+--     toggle_info       = '<S-Tab>',
+--     toggle_preview    = '<Tab>',
+-- }
--- readme.md
+++ readme.md
@@ -1,11 +1,95 @@
 # vanilla and sane config for neovim
 
-## todos
+## requirements
 
-- [ ] coloscheme and make treesitter working
-- [ ] auto format on save
-- [ ] incorporate mini.nvim and snacks.picker maybe?
+- neovim 0.12+
+- **lua, luajit**, lua-language-server
+- **go**, gopls
+- **rust**, rust-analyzer
+- **python**, pyright
+- **typescript**, typescript-language-server
+- **java, jdk**, jdtls
+- **csharp**, csharp-ls
+- **typst**, tinymist
+- **docker**, dockerls, compose-language-service
+- yaml-language-server
+- codebook
+
+## update all dependencies
+
+### windows
+
+```pwsh
+gup update && cargo install-update -a && scoop update -a && winget upgrade --all -u
+```
 
 ## references
 
 - <https://neovim.io/doc/user/lsp.html#lsp-config>
+- <https://gpanders.com/blog/whats-new-in-neovim-0-11/>
+- <https://www.youtube.com/watch?v=xGkL2N8w0H4>
+
+## keymap
+
+### Custom Keymaps
+
+These are the keybindings explicitly defined in the configuration files.
+
+| Keymap | Mode | Action | Description |
+| :--- | :--- | :--- | :--- |
+| ` ` (Space) | | `mapleader` | The leader key for custom shortcuts. |
+| `<leader>q` | Normal | `:quit<CR>` | Quits Neovim. |
+| `-` | Normal | `<CMD>Oil<CR>` | Opens the parent directory using oil.nvim. |
+| `<leader>b` | Normal | `vim.lsp.buf.format` | Formats the current buffer using the LSP. |
+| `<leader>f` | Normal | `MiniPick.builtin.files` | Finds files using mini.pick. |
+| `<leader>g` | Normal | `MiniPick.builtin.grep_live` | Performs a live grep search using mini.pick. |
+| `<leader>h` | Normal | `MiniPick.builtin.help` | Shows help for mini.pick. |
+| `<leader>p` | Normal | `:TypstPreviewToggle<CR>` | Toggles the typst-preview. |
+| `<C-Space>` | Insert | `vim.lsp.completion.get()` | Triggers LSP completion. |
+
+### Neovim LSP Default Keymaps (0.11+)
+
+These are the modern default keybindings that Neovim provides when an LSP client attaches to a buffer.
+
+| Keymap | Mode | Action | Description |
+| :--- | :--- | :--- | :--- |
+| `K` | Normal | `vim.lsp.buf.hover()` | Shows hover information for the symbol under the cursor. |
+| `gd` | Normal | `vim.lsp.buf.definition()` | Goes to the definition of the symbol under the cursor. |
+| `gD` | Normal | `vim.lsp.buf.declaration()` | Goes to the declaration of the symbol under the cursor. |
+| `gri` | Normal | `vim.lsp.buf.implementation()` | Lists all implementations for the symbol under the cursor. |
+| `gO` | Normal | `vim.lsp.buf.document_symbol()` | Shows a "table of contents" or outline of symbols in the current document. |
+| `go` | Normal | `vim.lsp.buf.type_definition()` | Goes to the type definition of the symbol under the cursor. |
+| `grr` | Normal | `vim.lsp.buf.references()` | Lists all references to the symbol under the cursor. |
+| `grn` | Normal | `vim.lsp.buf.rename()` | Renames all references to the symbol under the cursor. |
+| `gra` | Normal, Visual | `vim.lsp.buf.code_action()` | Selects a code action from a list of available actions. |
+| `CTRL-S` | Insert, Select | `vim.lsp.buf.signature_help()` | Displays signature information for the function being called. |
+
+### Diagnostic and List Navigation
+
+These keymaps are for navigating through diagnostics (errors, warnings) and various lists like quickfix and location lists.
+
+| Keymap | Action |
+| :--- | :--- |
+| `[d`, `]d` | Move between diagnostics in the current buffer. |
+| `[D`, `]D` | Jump to the first or last diagnostic in the buffer. |
+| `[q`, `]q` | Navigate through the quickfix list. |
+| `[Q`, `]Q` | Jump to the first or last item in the quickfix list. |
+| `[l`, `]l` | Navigate through the location list. |
+| `[L`, `]L` | Jump to the first or last item in the location list. |
+| `[b`, `]b` | Navigate through the buffer list. |
+| `[B`, `]B` | Jump to the first or last buffer in the list. |
+| `[t`, `]t` | Navigate through the tag matchlist. |
+| `[a`, `]a` | Navigate through the argument list. |
+
+### Insert and Completion Keymaps
+
+These are standard Neovim keybindings for completion in insert mode.
+
+| Keymap | Action | Description |
+| :--- | :--- | :--- |
+| `<C-x><C-o>` | `vim.lsp.omnifunc` | Triggers omni completion, typically powered by the LSP. |
+| `<C-n>` / `<C-p>` | Next/Previous Item | Navigates up and down the completion menu. |
+| `<C-e>` | End Completion | Stops completion without inserting the selected item. |
+| `<C-y>` | Accept Completion | Stops and accepts the currently selected completion item. |
+| `<C-x><C-f>` | File Path Completion | Completes file and directory names. |
+| `<C-x><C-l>` | Whole Line Completion | Completes entire lines based on other lines in the buffer. |
